ae64a3aa1b7f7efc9cf190c1d92597bb3aa5a31e
y-nagase <y-nagase@nagase-system.com>
2025-11-18 19:19:23 +0900

    style: add menubar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
 import { MicPermissionStatus } from '@/components/transcript/MicPermissionStatus';
 import { RecordingControls } from '@/components/transcript/RecordingControls';
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from '@/components/ui/menubar';
+import Link from 'next/link';
 
 export default function Page() {
   return (
     <>
+      <Menubar>
+        <MenubarMenu>
+          <MenubarTrigger>Pages</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>
+              <Link href={'/realtime-api'}>Realtime API</Link>
+            </MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
       <div className="mb-8 text-center">
         <h1 className="text-4xl font-bold tracking-tight">Voice Transcriber</h1>
         <p className="mt-2 text-muted-foreground">
